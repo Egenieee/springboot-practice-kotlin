64f3b4aac5db14f7f8ef3ba7ec55f26f7fcd8f04
Egenieee <hhgg0925@naver.com>
2022-08-25 22:09:25 +0900

    public이 기본 접근제한자이기 때문에 생략

--- src/main/kotlin/com/springboot/practice/web/HelloController.kt
+++ src/main/kotlin/com/springboot/practice/web/HelloController.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class HelloController {
     @GetMapping("/hello")
-    public fun hello() : String {
+    fun hello() : String {
         return "hello"
     }
 }
