a43429c1745a9ae624cf42b00627605f2a5eeec9
Egenieee <hhgg0925@naver.com>
2022-08-25 23:49:26 +0900

    reformat code

--- src/test/kotlin/com/springboot/practice/web/HelloControllerTest.kt
+++ src/test/kotlin/com/springboot/practice/web/HelloControllerTest.kt
@@ -1,15 +1,11 @@
 package com.springboot.practice.web
 
-import com.fasterxml.jackson.module.kotlin.jsonMapper
-import io.kotest.matchers.paths.exist
-import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
 @WebMvcTest
