0e422c4e336b0a49cecff72e52aaa44b620b9eb3
Egenieee <hhgg0925@naver.com>
2022-08-26 23:54:24 +0900

    fix error - @WebMvcTest annotation remove - @SpringBootTest annotation add

--- src/test/kotlin/com/springboot/practice/web/HelloControllerTest.kt
+++ src/test/kotlin/com/springboot/practice/web/HelloControllerTest.kt
@@ -3,13 +3,14 @@ package com.springboot.practice.web
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
-@WebMvcTest
+//@WebMvcTest
 @AutoConfigureMockMvc
+@SpringBootTest
 class HelloControllerTest (
     @Autowired
     private val mockMvc: MockMvc
