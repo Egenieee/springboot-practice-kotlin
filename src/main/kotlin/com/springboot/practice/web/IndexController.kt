3f8bc827f615a2caf1b448028dad0df5ef83bfd7
Egenieee <hhgg0925@naver.com>
2022-08-28 22:12:52 +0900

    feat. 기본적인 화면 생성 기능 추가 - 기본 적인 페이지 출력을 위한 mustache 파일 생성 - IndexController 생성

---
+++ src/main/kotlin/com/springboot/practice/web/IndexController.kt
@@ -0,0 +1,13 @@
+package com.springboot.practice.web
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class IndexController {
+
+    @GetMapping("/")
+    fun index(): String {
+        return "index"
+    }
+}
